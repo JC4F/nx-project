21cf3e7693b189c4aac618a360bdd152c1d29101
jc4f <lequan12122002@gmail.com>
2024-07-17 17:32:43 +0700

    add share spartan ui, wip other lib not work

---
+++ libs/shared/ui/others/src/lib/others.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'as-others',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './others.component.html',
+  styleUrl: './others.component.css',
+})
+export class OthersComponent {}
