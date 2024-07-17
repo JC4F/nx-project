820b0c181153c6d4e8eb61c51958a3ecd76eebf3
JC4F <lequan12122002@gmail.com>
2024-07-17 21:24:58 +0700

    change project name by using nx move

--- apps/spotify/jest.config.ts
+++ apps/spotify/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'angular-spotify',
+  displayName: 'spotify',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/angular-spotify',
+  coverageDirectory: '../../coverage/apps/spotify',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
